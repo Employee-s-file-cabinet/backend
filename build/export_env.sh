9a616f00ce0f69bbff6a891bc6fc04da0e8df302
PaulYakow <paul.yakow@gmail.com>
2023-12-25 15:27:59 +0500

    feat(actions): add export env variables

---
+++ build/export_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /srv || exit
+touch .env
+{
+  echo HTTP_PORT=${{ secrets.HTTP_PORT }}
+  HTTP_TOKEN_SECRET_KEY=${{ secrets.HTTP_TOKEN_SECRET_KEY }}
+  PG_DSN=${{ secrets.PG_DSN }}
+  S3_ACCESS_KEY_ID=${{ secrets.S3_ACCESS_KEY_ID }}
+  S3_SECRET_ACCESS_KEY=${{ secrets.S3_SECRET_ACCESS_KEY }}
+
+} >> .env
