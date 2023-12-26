807cbd5777ebf46627b375efe508d3e944bcc52b
Aleksandr Baranov <a.n.baranov@outlook.com>
2023-12-27 00:27:58 +0300

    refactor: services layer added model user updated, get user and list users handlers implemented, replaced sqlx to scany, replaced std pgx to native pgx pool

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,16 +3,16 @@ package config
 import (
 	"github.com/ilyakaznacheev/cleanenv"
 
-	"github.com/Employee-s-file-cabinet/backend/internal/server"
-	"github.com/Employee-s-file-cabinet/backend/internal/storage/db/postgresql"
-	"github.com/Employee-s-file-cabinet/backend/internal/storage/s3"
+	"github.com/Employee-s-file-cabinet/backend/internal/delivery/http"
+	repopg "github.com/Employee-s-file-cabinet/backend/internal/repo/postgresql"
+	repos3 "github.com/Employee-s-file-cabinet/backend/internal/repo/s3"
 )
 
 type Config struct {
-	LogLevel string            `env:"LOG_LEVEL" env-default:"debug"`
-	HTTP     server.Config     `env-prefix:"HTTP_"`
-	PG       postgresql.Config `env-prefix:"PG_"`
-	S3       s3.Config         `env-prefix:"S3_"`
+	LogLevel string        `env:"LOG_LEVEL" env-default:"debug"`
+	HTTP     http.Config   `env-prefix:"HTTP_"`
+	PG       repopg.Config `env-prefix:"PG_"`
+	S3       repos3.Config `env-prefix:"S3_"`
 }
 
 // New создаёт объект Config.
---
+++ internal/delivery/http/server.go
@@ -0,0 +1,94 @@
+package http
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+
+	srvErrors "github.com/Employee-s-file-cabinet/backend/internal/delivery/http/errors"
+	"github.com/Employee-s-file-cabinet/backend/internal/delivery/http/internal/api"
+	"github.com/Employee-s-file-cabinet/backend/internal/delivery/http/internal/handlers"
+	"github.com/Employee-s-file-cabinet/backend/internal/delivery/http/internal/middleware"
+)
+
+const baseURL = "/api/v1"
+
+const (
+	defaultIdleTimeout    = time.Minute
+	defaultReadTimeout    = 5 * time.Second
+	defaultWriteTimeout   = 10 * time.Second
+	defaultShutdownPeriod = 30 * time.Second
+)
+
+type server struct {
+	httpServer *http.Server
+	logger     *slog.Logger
+}
+
+func New(cfg Config,
+	userService handlers.UserService,
+	authService handlers.AuthService,
+	logger *slog.Logger) *server {
+	logger = logger.With(slog.String("from", "http-server"))
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.Port),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelWarn),
+		IdleTimeout:  defaultIdleTimeout,
+		ReadTimeout:  defaultReadTimeout,
+		WriteTimeout: defaultWriteTimeout,
+	}
+
+	s := &server{
+		httpServer: srv,
+		logger:     logger,
+	}
+
+	handler := handlers.New(userService, authService, logger)
+
+	mux := chi.NewRouter()
+	mux.NotFound(srvErrors.NotFound)
+	mux.MethodNotAllowed(srvErrors.MethodNotAllowed)
+	mux.Use(middleware.LogAccess)
+	mux.Use(middleware.RecoverPanic)
+
+	srv.Handler = api.HandlerWithOptions(handler, api.ChiServerOptions{
+		BaseURL:    baseURL,
+		BaseRouter: mux,
+	})
+
+	return s
+}
+
+func (s *server) Run(ctx context.Context) error {
+	shutdownErrorChan := make(chan error)
+
+	go func() {
+		<-ctx.Done()
+		ctx, cancel := context.WithTimeout(context.Background(), defaultShutdownPeriod)
+		defer cancel()
+
+		shutdownErrorChan <- s.httpServer.Shutdown(ctx)
+	}()
+
+	s.logger.Info("starting server", slog.String("addr", s.httpServer.Addr))
+
+	err := s.httpServer.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownErrorChan
+	if err != nil {
+		return err
+	}
+
+	s.logger.Info("stopped server", slog.String("addr", s.httpServer.Addr))
+
+	return nil
+}
